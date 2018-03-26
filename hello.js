ea2908a432059f3ca07821ba9b71bcdcbc13fa1f
amol-deole <amol.deole@zibtek.com>
2018-03-26 20:08:52 +0530

    This for checking

--- hello.js
+++ hello.js
@@ -4,6 +4,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n'); // missing semi-colon will fail the build
   res.end("Helloo Mr. Programmer Glad to see you here");
+  res.end("This is for testing and missed the semiconoal")
 }).listen(1337, '127.0.0.1');
  
 console.log('Server running at http://127.0.0.1:1337/');
